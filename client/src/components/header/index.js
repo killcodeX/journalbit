ff0b9becda041cf6dd1cdfa924b105d991e60f1a
Aaquib Ahmed <aaquibskimbox@gmail.com>
2021-08-10 13:59:25 +0530

    added styled comp

---
+++ client/src/components/header/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { HeaderWrapper } from './style'
+
+export default function Header() {
+    return (
+        <HeaderWrapper>
+            Header
+        </HeaderWrapper>
+    )
+}
---
+++ client/src/components/header/style.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+
+export const HeaderWrapper = styled.nav`
+    background-color:white;
+    padding:20px;
+    min-height:30px;
+`;
