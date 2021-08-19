72c7a440ac6b4569c4bb4b24fc55068432529398
Aaquib Ahmed <aaquibskimbox@gmail.com>
2021-08-19 11:46:29 +0530

    changed the folder name

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import Header from "./components/header";
 import Login from "./container/Auth/login";
 import Register from "./container/Auth/register";
 import Profile from "./container/UserProfile";
-import UserDetailForm from "./container/Auth/userDetailForm";
+import UserDetailForm from "./container/Auth/accsetting";
 import ProtectedRoutes from "./protectedRoutes";
 import "./App.css";
 
--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
-const postSchema = mongoose.Schema({
+const postSchema = new mongoose.Schema({
   fname: String,
   lname: String,
   image: String,
   email: String,
   password: String,
-  // bio: { type: String, default: "Hey! I am new to this Universe!" },
+  bio: { type: String, default: "Hey! I am new to this Universe!" },
   // posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
   // followers: [{ type: Schema.Types.ObjectId, ref: "User" }],
   // following: [{ type: Schema.Types.ObjectId, ref: "User" }],
